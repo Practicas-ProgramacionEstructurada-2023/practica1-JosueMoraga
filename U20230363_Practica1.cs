76a9a675fefde18b59f6d4e2b7a9c8f75aa0e051
JosueMoraga <jbenja2152@gmail.com>
2023-08-12 16:19:32 -0600

    Practica1

---
+++ U20230363_Practica1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            //Declaracion de variables
+            string? Nombre;
+            int edad;
+            string? direccion;
+            int telefono;
+            string? universidad;
+            string? carrera;
+            string? facultad;
+            int ciclo;
+            
+            Console.WriteLine("\n**********Practica 1 - Programacion estructurada**********");
+            Console.WriteLine("\n");
+            
+            Console.WriteLine("Ingrese su nombre completo");//obtener datos
+            Nombre = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese su edad");//obtener datos
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese su direccion");//obtener datos
+            direccion = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese su numero de telefono");//obtener datos
+            telefono = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese el nombre de la universidad en la que estudia");//obtener datos
+            universidad = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese el nombre de su carrera");//obtener datos
+            carrera = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese el nombre de su facultad");//obtener datos
+            facultad = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ingrese el ciclo en el que se encuentra");//obtener datos
+            ciclo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            //imprimiendo datos
+            
+
+
+
+
+        }
+    }
+}
